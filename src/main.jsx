7bc52d454417323f4a886c5276cb2141150aaadb
priyankasharma6785 <priyankasharma6785@gmail.com>
2026-06-17 17:00:33 +0530

    Move vercel.json to root

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import './assets/style.css';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
