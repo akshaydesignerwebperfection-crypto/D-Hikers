bb56e20e3f266f6727cc5147b8a74207e892046b
Akshay <akshaydesigner.webperfection@gmail.com>
2026-07-22 12:39:15 +0530

    mobile logo update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -93,8 +93,7 @@ export default function Header() {
           </button>
 
           <NavLink to="/" className="flex items-center gap-1 text-xl font-semibold text-slate-900">
-            <svg className="h-10 w-10" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="20" cy="22" rx="16" ry="10" fill="#e8442a" opacity="0.15"/><path d="M20 4 C10 14 6 22 20 28 C34 22 30 14 20 4Z" fill="#e8442a"/><path d="M20 4 C24 14 34 18 20 28" fill="#c93820" opacity="0.5"/><circle cx="20" cy="18" r="5" fill="white"/></svg>
-            D Hikers
+            <img className='w-16 h-16 object-cover' src={Logo} alt="Dhauladhar Hikers" />
           </NavLink>
 
           <div className="flex items-center gap-4">
