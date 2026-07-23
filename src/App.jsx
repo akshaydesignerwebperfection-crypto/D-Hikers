c879e7ef1c4ba9d213bb89fbd2ea50ede8c1430e
Akshay <akshaydesigner.webperfection@gmail.com>
2026-07-23 10:46:30 +0530

    update App file and main.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-﻿import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-import Header from './components/header';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Destination from './Pages/Destination';
-import Tour from './Pages/Tour';
-import Contact from './Pages/Contact';
-import News from './Pages/News';
-import Login from './Pages/Login';
-import MoroccoTourDetail from './Pages/Morocco-Tour-Detail';
-import UnitedKingdomTourDetail from './Pages/United-Kingdom-Tour-Detail';
-import Footer from './components/Footer';
+﻿import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+import Header from "./components/header";
+import Footer from "./components/Footer";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Destination from "./Pages/Destination";
+import Tour from "./Pages/Tour";
+import Contact from "./Pages/Contact";
+import News from "./Pages/News";
+import Login from "./Pages/Login";
+import MoroccoTourDetail from "./Pages/Morocco-Tour-Detail";
+import UnitedKingdomTourDetail from "./Pages/United-Kingdom-Tour-Detail";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -24,24 +26,33 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <ScrollToTop />
+
       <Header />
+
       <main className="main-wrapper">
         <Routes>
-           <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/destinations" element={<Destination />} />
-            <Route path="/tours" element={<Tour />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/morocco-tour-detail" element={<MoroccoTourDetail />} /> 
-            <Route path="/united-kingdom-tour-detail" element={<UnitedKingdomTourDetail />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/destinations" element={<Destination />} />
+          <Route path="/tours" element={<Tour />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/morocco-tour-detail"
+            element={<MoroccoTourDetail />}
+          />
+          <Route
+            path="/united-kingdom-tour-detail"
+            element={<UnitedKingdomTourDetail />}
+          />
         </Routes>
       </main>
+
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
